5408ca730feca1b4f48b2063f36c82caec1f4bc7
KimByeongHoon51 <byeoku501@naver.com>
2025-05-26 09:59:16 +0900

    Initial commit

---
+++ Source/L20250526_Input/L20250526_Input.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "L20250526_Input.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, L20250526_Input, "L20250526_Input" );
